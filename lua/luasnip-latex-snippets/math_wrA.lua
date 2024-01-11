d4082219812f0b63a9a65fbbb2c99097ea3e4ff9
Raymond Bian <raybbian@gmail.com>
2024-01-11 15:46:27 -0500

    upd

--- lua/luasnip-latex-snippets/math_wrA.lua
+++ lua/luasnip-latex-snippets/math_wrA.lua
@@ -15,6 +15,16 @@ local frac_no_parens = {
   i(0),
 }
 
+local cmb_no_parens = {
+  f(function(_, snip)
+    return string.format("\\binom{%s}", snip.captures[1])
+  end, {}),
+  t("{"),
+  i(1),
+  t("}"),
+  i(0),
+}
+
 local frac_node = {
   f(function(_, snip)
     local match = snip.trigger
@@ -46,6 +56,37 @@ local frac_node = {
   i(0),
 }
 
+local cmb_node = {
+  f(function(_, snip)
+    local match = snip.trigger
+    local stripped = match:sub(1, #match - 1)
+
+    i = #stripped
+    local depth = 0
+    while true do
+      if stripped:sub(i, i) == ")" then
+        depth = depth + 1
+      end
+      if stripped:sub(i, i) == "(" then
+        depth = depth - 1
+      end
+      if depth == 0 then
+        break
+      end
+      i = i - 1
+    end
+
+    local rv =
+      string.format("%s\\binom{%s}", stripped:sub(1, i - 1), stripped:sub(i + 1, #stripped - 1))
+
+    return rv
+  end, {}),
+  t("{"),
+  i(1),
+  t("}"),
+  i(0),
+}
+
 local subscript_node = {
   f(function(_, snip)
     return string.format("%s_{%s}", snip.captures[1], snip.captures[2])
@@ -58,7 +99,17 @@ local frac_no_parens_triggers = {
   "(\\?[%w]+\\?_%w)/",
   "(\\?[%w]+\\?^{%w*})/",
   "(\\?[%w]+\\?_{%w*})/",
+  "(\\?%w+)!/",
   "(\\?%w+)/",
+}
+
+local cmb_no_parens_triggers = {
+  "(\\?[%w]+\\?^%w)cmb",
+  "(\\?[%w]+\\?_%w)cmb",
+  "(\\?[%w]+\\?^{%w*})cmb",
+  "(\\?[%w]+\\?_{%w*})cmb",
+  "(\\?%w+)!cmb",
+  "(\\?%w+)cmb",
 }
 
 function M.retrieve(is_math)
@@ -88,6 +139,13 @@ function M.retrieve(is_math)
       name = "() frac",
       wordTrig = true,
     }, vim.deepcopy(frac_node)),
+
+    s({
+      priority = 1000,
+      trig = ".*%)/",
+      name = "() cmb",
+      wordTrig = true,
+    }, vim.deepcopy(cmb_node)),
   }
 
   for _, trig in pairs(frac_no_parens_triggers) do
@@ -95,6 +153,13 @@ function M.retrieve(is_math)
       name = "Fraction no ()",
       trig = trig,
     }, vim.deepcopy(frac_no_parens))
+  end
+
+  for _, trig in pairs(cmb_no_parens_triggers) do
+    snippets[#snippets + 1] = s({
+      name = "Fraction no ()",
+      trig = trig,
+    }, vim.deepcopy(cmb_no_parens))
   end
 
   return snippets
